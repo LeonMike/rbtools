529ca61a1e3c11a0718f76e7ac90096967aaa738
Ronny Barrera <r.obed.barrera@gmail.com>
2020-01-04 00:04:33 -0600

    + Started toolbars project

---
+++ spec/rbtools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RbtoolsView from '../lib/Views/rbtools-view';
+
+describe('RbtoolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
